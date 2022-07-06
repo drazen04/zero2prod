293319fafe34522e23620ef66b3543488a109925
Drazen04 <hangtime23@hotmail.it>
2022-07-06 20:10:50 +0200

    remove async\await logic.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse};
+use actix_web::dev::Server;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-#[tokio::main]
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/healt_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/healt_check", web::get().to(health_check))
+            })
+            .bind("127.0.0.1:8000")?
+            .run();
+    Ok(server)
 }
