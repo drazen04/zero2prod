c29467a9a50fd3899fb47d1d15f8ddad1582250a
Drazen04 <hangtime23@hotmail.it>
2022-07-06 19:42:59 +0200

    move logic in lib file.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/healt_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/healt_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
